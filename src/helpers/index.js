90bf91f3389a42d7b99b6323332f331b6523f2af
joeeverett4 <joe.everett34@gmail.com>
2022-06-29 21:51:35 +0100

    msg

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,26 +6,7 @@
     
     
 
-    
-  /*  const res = await fetch("/deletemeta", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(metafieldsToDelete),
-    });
 
-    const response = await fetch("/createmeta", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(metafieldsToCreate),
-    });
-*/
-    
     
   };
   
